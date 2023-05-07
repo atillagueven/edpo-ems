da3ec3b1d41d7fd33041dd066f2f5a2832721843
V4L3 <V4L3@users.noreply.github.com>
2023-05-07 17:06:05 +0200

    Added scenarios to README.md

--- frontend/src/layouts/default/AppBar.vue
+++ frontend/src/layouts/default/AppBar.vue
@@ -1,9 +1,11 @@
 <template>
   <v-app-bar flat>
     <v-app-bar-title>
+      <a href="/">
       <v-icon icon="mdi-circle-slice-4" />
 
       EMSai
+      </a>
     </v-app-bar-title>
   </v-app-bar>
 </template>
